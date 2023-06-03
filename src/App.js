86e7a1bdcf7fe3ab1a1c40b7b483b47f4391b1f4
AlexC3105 <126286847+AlexC3105@users.noreply.github.com>
2023-06-02 22:06:38 -0400

    updated code

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
